90bf05646c2612f83acdf5453cbc677c87dad709
StormyMeadowlark <ashlee@stormymeadowlark.com>
2024-08-26 10:27:36 -0500

    upgarding

--- middleware/attachTenant.js
+++ middleware/attachTenant.js
@@ -1,9 +1,7 @@
-// middleware/attachTenant.js
 module.exports = (req, res, next) => {
-  const tenantId =
-    req.params.tenantId || req.body.tenantId || req.query.tenantId;
+  const tenantId = req.headers["x-tenant-id"];
   if (!tenantId) {
-    return res.status(400).json({ message: "Tenant ID is required" });
+    return res.status(400).json({ message: "X-Tenant-Id header is required" });
   }
   req.tenantId = tenantId;
   next();
